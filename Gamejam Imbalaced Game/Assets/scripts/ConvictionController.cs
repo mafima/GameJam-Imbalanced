60161f56ac82bd0f18716128841626900f97a7ad
Unknown <S_Eisenspan@web.de>
2018-11-17 00:45:39 +0100

    Added Conviction for Player
    Conviction, Level and Transformation technically implemented with public functions for conviction gain

---
+++ Gamejam Imbalaced Game/Assets/scripts/ConvictionController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConvictionController : MonoBehaviour {
+
+    private float transformTime = 2f;
+
+    private int goodness = 0;
+    private int badness = 0;
+
+    public Text levelText;
+    public Slider goodSlider;
+    public Slider badSlider;
+    private Color sliderBgColor = new Color(0.674f, 0.937f, 1f);
+    private Color sliderGlowColor = new Color(0.968f, 0.986f, 0.219f);
+
+    private int level = 1;
+    private int goodLvl = 1;
+    private int badLvl = 1;
+
+    bool transformed = false;
+    Queue<float> toBeTransformed = new Queue<float>();
+
+    private void Start() {
+        goodSlider.maxValue = 1f;
+        goodSlider.value = 0f;
+        goodSlider.wholeNumbers = false;
+
+        badSlider.maxValue = 1f;
+        badSlider.value = 0f;
+        badSlider.wholeNumbers = false;
+
+        GenerateConviction(true, 20);
+        GenerateConviction(false, 20);
+    }
+
+    public void GenerateConviction(bool good, int amount) {
+        if (good) {
+            goodness += amount * goodLvl;
+        } else {
+            badness += amount * badLvl;
+        }
+        RedrawBars();
+    }
+
+    private void RedrawBars() {
+        Vector2 temp = new Vector2(goodness, badness);
+        temp.Normalize();
+        float str = (goodness + badness) / (level * 100f);
+        goodSlider.value = temp.x * str;
+        badSlider.value = temp.y * str;
+        if (goodSlider.value >= 1f) {
+            LevelUp(true);
+        } else if (badSlider.value >= 1f) {
+            LevelUp(false);
+        }
+    }
+
+    private void LevelUp(bool good) {
+        goodness = badness = 0;
+        level++;
+        levelText.text = level.ToString();
+        if (good) {
+            badLvl++;
+            toBeTransformed.Enqueue(transformTime * goodLvl);
+            TransformationQueuer();
+            if (badLvl >= goodLvl + 3) {
+                badSlider.transform.Find("Background").GetComponent<Image>().color = sliderGlowColor;
+            } else {
+                badSlider.transform.Find("Background").GetComponent<Image>().color = sliderBgColor;
+            }
+        } else {
+            goodLvl++;
+            toBeTransformed.Enqueue(-transformTime * badLvl);
+            TransformationQueuer();
+            if (goodLvl >= badLvl + 3) {
+                goodSlider.transform.Find("Background").GetComponent<Image>().color = sliderGlowColor;
+            } else {
+                goodSlider.transform.Find("Background").GetComponent<Image>().color = sliderBgColor;
+            }
+        }
+        GenerateConviction(good, 25);
+    }
+
+    private void TransformationQueuer() {
+        if (transformed || toBeTransformed.Count == 0) {
+            return;
+        }
+        float next = toBeTransformed.Dequeue();
+        if (next > 0f) {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+            StartCoroutine(TransformBack(next));
+        } else {
+            next *= -1f;
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+            StartCoroutine(TransformBack(next));
+        }
+    }
+
+    IEnumerator TransformBack(float timer) {
+        transformed = true;
+        yield return new WaitForSeconds(timer);
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+        transformed = false;
+        TransformationQueuer();
+    }
+}
