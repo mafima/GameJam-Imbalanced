05905ff8e9d5de7782824f14f707280f3fc0eb84
Asuriuz <S_Eisenspan@web.de>
2018-11-17 17:42:42 +0100

    Optional terrainmaker

--- Gamejam Imbalaced Game/Assets/Scripts/FPS Basics/NetworkManager.cs
+++ Gamejam Imbalaced Game/Assets/Scripts/FPS Basics/NetworkManager.cs
@@ -62,7 +62,7 @@ public class NetworkManager : Photon.MonoBehaviour {
         connectionText.text = "";
         StartSpawnProcess(0.0f);
 
-        if (PhotonNetwork.playerList.Length >= 2) {
+        if (PhotonNetwork.playerList.Length >= 2 && terrainMaker) {
             Debug.Log("Making");
             terrainMaker.Make();
         }
