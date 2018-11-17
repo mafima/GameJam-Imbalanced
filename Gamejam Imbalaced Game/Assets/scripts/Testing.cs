58c7d142f2a7da9e9a3b6ffc1be15d9f01a46116
Manuel <manumond@web.de>
2018-11-17 22:10:10 +0100

    impl sounds

--- Gamejam Imbalaced Game/Assets/scripts/Testing.cs
+++ Gamejam Imbalaced Game/Assets/scripts/Testing.cs
@@ -14,6 +14,7 @@ public class Testing : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(!cc)return;
         if (Input.GetKeyDown("q")) {
             cc.GenerateConviction(true, 5);
         }
