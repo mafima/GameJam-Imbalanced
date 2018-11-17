8f6db41b3889a361b392813b3024790d43407091
Theo <geometheo@yahoo.de>
2018-11-18 00:40:38 +0100

    Enemy fixing Part 3

--- Gamejam Imbalaced Game/Assets/scripts/Enemy.cs
+++ Gamejam Imbalaced Game/Assets/scripts/Enemy.cs
@@ -22,10 +22,10 @@ public class Enemy : MonoBehaviour {
 
 		for (int i = 0; i < EnemyManager.playerTf.Length; i++) {
 			if (dist == 0) {
-				dist = (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z + enemyTf.position.z), 2f));
+				dist = (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z - enemyTf.position.z), 2f));
 				nearestPlayer = i;
-			} else if (dist > (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z + enemyTf.position.z), 2f))) {
-				dist = (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z + enemyTf.position.z), 2f));
+			} else if (dist > (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z - enemyTf.position.z), 2f))) {
+				dist = (Mathf.Pow ((EnemyManager.playerTf[i].position.x - enemyTf.position.x), 2f) + Mathf.Pow ((EnemyManager.playerTf[i].position.z - enemyTf.position.z), 2f));
 				nearestPlayer = i;
 			}
 		}
